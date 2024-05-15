1697c732e8eeba99883b29efaa96e4d0cb175c2e
Ilia Liubinskii <ilia.liubinskii@gmail.com>
2024-05-15 09:15:43 +0300

    Added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 function App() {
   return (
-    <></>
+    <div className="w-full h-screen bg-slate-300 text-slate-500 flex justify-center items-center text-xl uppercase font-bold">
+      Starter
+    </div>
   )
 }
 
